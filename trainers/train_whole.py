2d58b664989e0c6644b395d21d17b6bf0e166598
lzl <875159954@qq.com>
2024-07-16 19:13:13 +0800

    1.modify dataset loading to load all first. 2.change code to use higher version of scipy.spatial.transform.

---
+++ trainers/train_whole.py
@@ -0,0 +1,97 @@
+import torch
+import sys
+import toml
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.tensorboard import SummaryWriter   
+
+from Network.VOFlowNet import VOFlowRes as FlowPoseNet
+
+from utils.train_pose_utils import load_from_file,load_model, save_model, train_pose_batch, validate
+import argparse
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config',  default='configs/train_test.toml')
+    config_file = args.parse_args().config
+    
+    with open(config_file, 'r') as file:
+        config = toml.load(file)
+        
+
+    datastr = config['datastr']
+    dataset_path = config['dataset_path']
+    batch_size = int(config['batch_size'])
+    worker_num = int(config['worker_num'])
+
+    model_name = config['model_name']
+    summary_path = config['summary_path']
+    model_path  = config['model_path']
+    image_width = int(config['image_width'])
+    image_height = int(config['image_height'])
+    flow_only = config['flow_only']
+    rcr_type = config['rcr_type']
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    def lr_lambda(iteration):
+        if iteration < 0.5 * total_iterations:
+            return 1.0
+        elif iteration < 0.875 * total_iterations:
+            return 0.2
+        else:
+            return 0.04
+    
+    iteration = 0 
+    num_epochs = 9999
+    total_iterations = int(config['total_iterations'])
+    learning_rate = float(config['learning_rate'] )
+    
+    model = FlowPoseNet()
+    model = torch.nn.DataParallel(model).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = LambdaLR(optimizer, lr_lambda)
+    summaryWriter = SummaryWriter(summary_path)
+    start_epoch,iteration = load_model(model, optimizer, scheduler, model_name)
+    model = model.module
+
+
+    train_scene_dataloaders = {}
+    with open(dataset_path, 'r') as f:
+        posefiles = f.readlines()
+    posefiles = [posefile.strip() for posefile in posefiles]
+    for posefile in posefiles:
+        train_scene_dataloaders[posefile] = load_from_file(posefile, datastr, image_height, image_width, batch_size, worker_num, flow_only=flow_only, rcr_type=rcr_type)
+    step = 1000
+    
+    for epoch in range(start_epoch,num_epochs):
+        for posefile in posefiles:
+            for sample in train_scene_dataloaders[posefile]:
+                if step < 10:
+                    step += 1
+                    print(f"train epoch {epoch}, iteration {iteration}")
+                    if iteration >= total_iterations:
+                        print(f"Successfully completed training for {iteration} iterations")
+                        sys.exit()
+                    sample = {k: v.to(device) for k, v in sample.items()} 
+                    total_loss,trans_loss,rot_loss = train_pose_batch(model, optimizer, sample )
+                    iteration += 1
+                    scheduler.step()
+                    if iteration % 10 == 0:
+                        summaryWriter.add_scalar('Loss/train_pose', total_loss, iteration)
+                        summaryWriter.add_scalar('Loss/train_trans', trans_loss, iteration)
+                        summaryWriter.add_scalar('Loss/train_rot', rot_loss, iteration)
+                        print(f"Epoch {epoch }, Step {iteration}, Loss: {total_loss}")
+                        print(f"translation loss: {trans_loss}, rotation loss: {rot_loss}")
+        if step >= 10:
+            break
+                    
+        # model_save_path = f'{model_path}/flowpose_model_iteration_{iteration}.pth'
+        # save_model(model, optimizer, scheduler, epoch, iteration, model_save_path)
+
+
+
+        
+
+
+
---
+++ utils/train_whole_utils.py
@@ -0,0 +1,155 @@
+import torch
+from Datasets.utils import ToTensor,  CropCenter, dataset_intrinsics, DownscaleFlow, Compose,RandomCropAndResized,ConsistentRandomResizedCrop
+from Datasets.tartanDataset import TartanDataset
+
+from torch.utils.data import DataLoader
+
+def train_pose_batch(model, optimizer, sample):
+    model.train()
+    # inputs-------------------------------------------------------------------
+    flow_gt = sample['flow']
+    intrinsic_gt = sample['intrinsic']
+    
+    # forward------------------------------------------------------------------
+    optimizer.zero_grad()  # Zero the parameter gradients
+    flow_input = torch.cat( ( flow_gt, intrinsic_gt ), dim=1 ) 
+    relative_motion = model(flow_input)
+
+    # loss calculation---------------------------------------------------------
+    motions_gt = sample['motion']
+    total_loss,trans_loss,rot_loss = calculate_loss(relative_motion, motions_gt)
+
+    # backpropagation----------------------------------------------------------
+    total_loss.backward()
+
+    optimizer.step()
+
+    return total_loss,trans_loss,rot_loss
+    
+def load_from_file(posefile,datastr,height,width,batch_size,worker_num,flow_only=True,rcr_type="NO_RCR"):
+    focalx, focaly, centerx, centery = dataset_intrinsics(datastr) 
+    transform = Compose([CropCenter((height,width)), DownscaleFlow(), ToTensor()])
+    if rcr_type == "RCR":
+        transform = Compose([CropCenter((height,width)),DownscaleFlow(), ToTensor(),RandomCropAndResized()])
+    elif rcr_type == "CONSISTENT_RCR":
+        transform = Compose([CropCenter((height,width)),DownscaleFlow(), ToTensor(),ConsistentRandomResizedCrop()])
+        
+    dataset = TartanDataset( posefile = posefile, transform=transform, 
+                                                focalx=focalx, focaly=focaly, centerx=centerx, centery=centery,flow_only=flow_only)
+    dataloader= DataLoader(dataset, batch_size=batch_size,
+                                                shuffle=False, num_workers=worker_num)
+    return dataloader
+def test_pose_batch(model, sample):
+    model.eval()
+    # inputs-------------------------------------------------------------------
+    flow_gt = sample['flow']
+    motions_gt = sample['motion']
+    intrinsic_gt = sample['intrinsic']
+    # forward------------------------------------------------------------------
+    with torch.no_grad():
+        # batch shapes are [batch_size,channels, Height,Width]
+        # So concant on dimension 1 not 0
+        flow_input = torch.cat((flow_gt, intrinsic_gt), dim=1)
+        relative_motion = model(flow_input)
+        total_loss,trans_loss,rot_loss = calculate_loss(relative_motion, motions_gt)
+       
+    # relative_motion = relative_motion.cpu().numpy()
+    # if 'motion' in sample:
+    #     motions_gt = sample['motion'].cpu().numpy()
+    #     scale = np.linalg.norm(motions_gt[:,:3], axis=1)
+    #     trans_est = relative_motion[:,:3]
+    #     trans_est = trans_est/np.linalg.norm(trans_est,axis=1).reshape(-1,1)*scale.reshape(-1,1)
+    #     relative_motion[:,:3] = trans_est 
+    # else:
+    #     print('    scale is not given, using 1 as the default scale value..')
+
+    return total_loss,trans_loss,rot_loss
+
+
+
+def calculate_loss( relative_motion, motions_gt):
+    
+    # Translation loss with normalization
+    epsilon = 1e-6
+    T_pred = relative_motion[:, :3]
+    T_gt = motions_gt[:, :3]
+    T_pred_norm = T_pred / torch.max(torch.norm(T_pred, dim=1, keepdim=True), torch.tensor(epsilon).cuda())
+    T_gt_norm = T_gt / torch.max(torch.norm(T_gt, dim=1, keepdim=True), torch.tensor(epsilon).cuda())
+    trans_loss = torch.nn.functional.mse_loss(T_pred_norm, T_gt_norm)
+    
+    # Simple Rotation loss
+    R_pred = relative_motion[:, 3:]
+    R_gt = motions_gt[:, 3:]
+    rot_loss = torch.nn.functional.mse_loss(R_pred, R_gt)
+
+    # Overall motion loss
+    pose_loss = trans_loss + rot_loss
+
+
+    return pose_loss,trans_loss,rot_loss
+
+
+
+def save_model(model, optimizer, scheduler, epoch, iteration, filepath):
+    """
+    保存模型、优化器和调度器的状态
+
+    参数:
+    model (torch.nn.Module): 要保存的模型
+    optimizer (torch.optim.Optimizer): 要保存的优化器
+    scheduler (torch.optim.lr_scheduler._LRScheduler): 要保存的调度器
+    epoch (int): 当前的epoch
+    iteration (int): 当前的迭代步数
+    filepath (str): 保存文件的路径
+    """
+    torch.save({
+        'epoch': epoch,
+        'iteration': iteration,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler_state_dict': scheduler.state_dict(),
+    }, filepath)
+def load_model(model, optimizer=None, scheduler=None, filepath=""):
+    """
+    加载模型、优化器和调度器的状态
+
+    参数:
+    model (torch.nn.Module): 要加载状态的模型
+    optimizer (torch.optim.Optimizer): 要加载状态的优化器
+    scheduler (torch.optim.lr_scheduler._LRScheduler): 要加载状态的调度器
+    filepath (str): 要加载文件的路径
+
+    返回:
+    int: 加载的epoch
+    int: 加载的迭代步数
+    """
+    if filepath=="":
+        return 0,0
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    if optimizer is not None:
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    if scheduler is not None:
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    epoch = checkpoint['epoch']  # 如果epoch没保存，默认值为0
+    iteration = checkpoint['iteration']
+    print(f"successfully load model from {filepath}")
+    return epoch+1, iteration
+
+def validate(model, testposefiles, device,RCR=False ):
+    model.eval()
+    N = 0
+    total_loss,trans_loss,rot_loss = 0,0,0
+    for posefile in testposefiles:
+        posefile = posefile.strip()
+        testdataiter = load_from_file(posefile, datastr='tartanair', height=448, width=640, batch_size=100, worker_num=16,RCR=RCR)
+        for sample in testdataiter:
+            sample = {k: v.to(device) for k, v in sample.items()}
+            total,trans,rot = test_pose_batch(model, sample )
+            N += 1   
+            total_loss += total.item()  # Ensure losses are scalars, not tensors
+            trans_loss += trans.item()
+            rot_loss += rot.item()
+            
+    return total_loss/N,trans_loss/N,rot_loss/N
+            
